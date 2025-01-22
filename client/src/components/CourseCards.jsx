dfe31b4d77d0d6b7e1ab9155820b15493bb0b741
VibhaeoM01 <vibhaeom1105@gmail.com>
2025-01-22 11:43:33 +0530

    error resolved

--- client/src/components/CourseCards.jsx
+++ client/src/components/CourseCards.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import CourseCard from './CourseCard';
 
 const val = [
-  { weeks: 7, Cname: 'WordPress For Everyone: Unlock Your Creativity', lessons: 70, students: 1000, src: 'Images/12.png' },
-  { weeks: 6, Cname: 'The Data Science Revolution: Upgrading Your Skills', lessons: 60, students: 900, src: 'Images/13.png' },
-  { weeks: 5, Cname: 'From Zero to Website: A Web Development Adventure', lessons: 50, students: 800, src: 'Images/14.png' },
-  { weeks: 4, Cname: 'advanced Mastering UI/UX: Design Fundamentals course', lessons: 40, students: 700, src: 'Images/15.png' },
+  { weeks: 7, Cname: 'WordPress For Everyone: Unlock Your Creativity', lessons: 70, students: 1000, src: '/src/Images/12.png' },
+  { weeks: 6, Cname: 'The Data Science Revolution: Upgrading Your Skills', lessons: 60, students: 900, src: '/src/Images/13.png' },
+  { weeks: 5, Cname: 'From Zero to Website: A Web Development Adventure', lessons: 50, students: 800, src: '/src/Images/14.png' },
+  { weeks: 4, Cname: 'advanced Mastering UI/UX: Design Fundamentals course', lessons: 40, students: 700, src: '/src/Images/15.png' },
 ];
 
 function CourseCards() {
@@ -16,7 +16,7 @@ function CourseCards() {
       
 
       {/* Existing content */}
-      <div className="flex flex-wrap w-2/3 gap-16 justify-center z-10">
+      <div className="flex flex-wrap  gap-16 justify-center z-10">
         {val.map((val) => (
           <CourseCard
             weeks={val.weeks}
