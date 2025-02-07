a95baeec533a237b359ccf9e9a07b40f570d2652
Sujal <palsujal707@gmail.com>
2025-02-07 15:05:01 +0530

    image path

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -4,14 +4,14 @@ import Card from './Card';
 // import BasicModal from './Modal';
 
 const cardData = [
-  { name: 'Web Development', clr: 'bg-cyan-500' ,bclr:'bg-cyan-200', im:'/public/Images/b1.png'},
-  { name: 'Data Science', clr: 'bg-pink-300' ,bclr:'bg-pink-200', im:'/public/Images/b2.png' },
-  { name: 'AI', clr: 'bg-green-500' ,bclr:'bg-green-200' ,im:'/public/Images/b3.png'},
-  { name: 'UI/UX', clr: 'bg-red-500' ,bclr:'bg-red-200',im:'/public/Images/b4.png' },
-  { name: 'Java', clr: 'bg-gray-500'  ,bclr:'bg-gray-200', im:'/public/Images/b2.png'},
-  { name: 'Python', clr: 'bg-yellow-500' ,bclr:'bg-yellow-200' ,im:'/public/Images/b2.png'},
-  { name: 'Cyber Security', clr: 'bg-blue-500' ,bclr:'bg-blue-200' ,im:'/public/Images/b2.png'},
-  { name: 'Machine Learning', clr: 'bg-orange-500' ,bclr:'bg-orange-200' ,im:'/public/Images/b5.png'},
+  { name: 'Web Development', clr: 'bg-cyan-500' ,bclr:'bg-cyan-200', im:'/Images/b1.png'},
+  { name: 'Data Science', clr: 'bg-pink-300' ,bclr:'bg-pink-200', im:'/Images/b2.png' },
+  { name: 'AI', clr: 'bg-green-500' ,bclr:'bg-green-200' ,im:'/Images/b3.png'},
+  { name: 'UI/UX', clr: 'bg-red-500' ,bclr:'bg-red-200',im:'/Images/b4.png' },
+  { name: 'Java', clr: 'bg-gray-500'  ,bclr:'bg-gray-200', im:'/Images/b2.png'},
+  { name: 'Python', clr: 'bg-yellow-500' ,bclr:'bg-yellow-200' ,im:'/Images/b2.png'},
+  { name: 'Cyber Security', clr: 'bg-blue-500' ,bclr:'bg-blue-200' ,im:'/Images/b2.png'},
+  { name: 'Machine Learning', clr: 'bg-orange-500' ,bclr:'bg-orange-200' ,im:'/Images/b5.png'},
 ];
 
 function Cards() {
